04b87e0dfe4a9aa0f2f1c1ec078a576574f4401e
zcode-dev <amsharidwan.tkg@gmail.com>
2025-02-04 16:00:01 +0700

    add navbar component

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,12 @@
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">LEFT</div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">RIGHT</div>
+    </div>
+  );
 };
 
 export default AdminPage;
